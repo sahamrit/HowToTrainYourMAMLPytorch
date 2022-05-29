58e261c609203ff1adb8225e7b059debc2497da8
sahamrit <amritsahu.iitkgp@gmail.com>
2022-05-29 17:34:47 +0000

    Adding Config NameSpace

---
+++ configs/conf.py
@@ -0,0 +1,55 @@
+"""TODO
+"""
+
+import os
+import json
+
+from pathlib import Path
+from types import SimpleNamespace
+
+class ExperimentConfig(SimpleNamespace):
+    def __init__(self) -> None:
+        
+        self.dataset = SimpleNamespace()
+        self.model = SimpleNamespace()
+        self.training = SimpleNamespace()
+
+
+        #General Configs 
+        self.seed = 0
+
+        #Dataset Configs
+        self.dataset.root_path = "/home/azureuser/cloudfiles/code/Users/asahu.cool/Work/Datasets/MiniImageNet/mini_imagenet_full_size"
+        self.dataset.train_path = "/home/azureuser/cloudfiles/code/Users/asahu.cool/Work/Datasets/MiniImageNet/mini_imagenet_full_size/train"
+        self.dataset.test_path = "/home/azureuser/cloudfiles/code/Users/asahu.cool/Work/Datasets/MiniImageNet/mini_imagenet_full_size/test"
+        self.dataset.val_path = "/home/azureuser/cloudfiles/code/Users/asahu.cool/Work/Datasets/MiniImageNet/mini_imagenet_full_size/val"
+        self.dataset.classmap_path = "/home/azureuser/cloudfiles/code/Users/asahu.cool/Work/Datasets/MiniImageNet/classmap.csv"
+
+        #Model Configs
+        self.model.torch_home = "/home/azureuser/cloudfiles/code/Users/asahu.cool/Work/Models"
+
+        #Training Configs
+        self.training.inner_loop_steps = 4
+        self.training.query_samples_per_class = 2
+        self.training.N_way = 5
+        self.training.K_shot = 5
+        self.training.batch_size = 16
+        self.training.image_size = (128,128)
+        self.training.gpu = True
+        self.training.lr = 0.01
+        self.training.n_epochs = 1
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
--- utils/generic_utils.py
+++ utils/generic_utils.py
@@ -4,35 +4,9 @@
 import os
 import matplotlib.pyplot as plt
 import numpy as np
-import pprint
 
 from pathlib import Path
 from typing import *
-
-
-def print_config(config_dict: dict, debug: bool = False) -> None:
-    """Print config dictionary.If name of a key ends with 
-    _debug then it is only printed if debug flag is set
-
-    Parameters
-    ----------
-        config_dict: dict
-            Config Dictionary containing data directories and other 
-            experiment configs. If name of a key ends with _debug
-            then it is only printed if debug flag is set
-        debug: bool
-            Flag for is debug mode
-    """
-    print("Config Dictionary \n")
-    for key in sorted(config_dict):
-        if key.split("_")[-1].lower() == "debug" and not debug:
-            continue
-        if type(config_dict[key]) == dict:
-            print(f"{key} => ")
-            pprint.pprint(config_dict[key],width=1)
-        else:
-            print(f"{key} => {config_dict[key]}")
-
 
 def class_distribution(root_path : str, classes : List[str]) -> dict:
     """TODO
